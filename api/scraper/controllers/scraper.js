9f4521468ed289ac75673f7e7eae46ccb89902c2
Andrew Dinh <andrewd@benchling.com>
2021-01-31 00:47:37 -0800

    Update the Scraper findOne controller

--- api/scraper/controllers/scraper.js
+++ api/scraper/controllers/scraper.js
@@ -1,8 +1,21 @@
-'use strict';
+const { sanitizeEntity } = require('strapi-utils');
 
 /**
  * Read the documentation (https://strapi.io/documentation/developer-docs/latest/concepts/controllers.html#core-controllers)
  * to customize this controller
  */
 
-module.exports = {};
+module.exports = {
+  /**
+   * Retrieve a record.
+   *
+   * @return {Object}
+   */
+
+  async findOne(ctx) {
+    const { slug } = ctx.params;
+
+    const entity = await strapi.services.scraper.findOne({ slug });
+    return sanitizeEntity(entity, { model: strapi.models.scraper });
+  },
+};
