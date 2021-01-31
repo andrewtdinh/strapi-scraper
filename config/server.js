4b1f9084a8d358fafb95721857e962304ff33191
Andrew Dinh <andrewd@benchling.com>
2021-01-30 22:19:09 -0800

    Commit all files

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '9843dc813003a8a4185377be1dd7630c'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'fb569074-23e2-45bd-9814-b89ec288be84'
+};
