058981fae7c449197119fd2e5f3ac2ed4edce634
Andrew Dinh <andrewd@benchling.com>
2021-01-31 23:40:39 -0800

    Update the jamstack.js file

--- scripts/scrapers/jamstack.js
+++ scripts/scrapers/jamstack.js
@@ -1,3 +1,22 @@
+'use strict'
+
+const chalk = require('chalk');
+const puppeteer = require('puppeteer');
+const {
+  getReport,
+  getDate,
+  getAllSG,
+  scraperCanRun
+} = require('./utils/utils.js')
+
+let report = {}
+let errors = []
+let newSG = 0
+
+const scrape = async () => {
+  console.log("Scrape function");
+}
+
 const main = async () => {
   // Fetch the correct scraper thanks to the slug
   const slug = "jamstack-org"
@@ -5,11 +24,18 @@ const main = async () => {
     slug: slug
   });
 
-  console.log(scraper);
-
   // If the scraper doesn't exists, is disabled or doesn't have a frequency then we do nothing
-  if (scraper == null || !scraper.enabled || !scraper.frequency)
+  if (scraper == null || !scraper.enabled || !scraper.frequency){
+    console.log(`${chalk.red("Exit")}: (Your scraper may does not exist, is not activated or does not have a frequency field filled in)`);
     return
+  }
+
+  const canRun = await scraperCanRun(scraper);
+  if (canRun && scraper.enabled){
+    const allSG = await getAllSG(scraper)
+    await scrape(allSG, scraper)
+  report = await getReport(newSG);
+  }
 }
 
 exports.main = main;
